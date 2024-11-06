aaf0ce255716ab963d6bb42fbd83fb6ff61d3b43
locluklak <dtanloc.j@gmail.com>
2024-11-06 11:04:25 +0700

    modify last commit

--- plot/plotFPR_AIC.py
+++ plot/plotFPR_AIC.py
@@ -5,13 +5,13 @@ sample_sizes = [50, 100, 150, 200]
 
 no_inf = [1.0, 1.0, 1.0, 1.0]
 naiveAIC = [0.141666666666, 0.10833333333334, 0.10833333333334, 0.0833333333]
-ocfixedk = [0.04166667, 0.033333333, 0.0583333333, 0.0416666666]
+ocAIC = [0.04166667, 0.033333333, 0.0583333333, 0.0416666666]
 parafixedk = [0.0667, 0.05, 0.075, 0]
 
 # Plot each series with different colors and markers
 plt.plot(sample_sizes, no_inf, label='No inference', marker='o', color='magenta')
 plt.plot(sample_sizes, naiveAIC, label='Naive', marker='o', color='red')
-plt.plot(sample_sizes, ocfixedk, label='oc-fixedK', marker='o', color='green')
+plt.plot(sample_sizes, ocAIC, label='oc-AIC', marker='o', color='green')
 # plt.plot(sample_sizes, parafixedk, label='para-fixedK', marker='o', color='blue')
 
 
